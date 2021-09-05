5608d3e9a1587a5f9998ef899bbf7fe521dd241b
alicekananda <alicekananda@gmail.com>
2021-09-04 21:50:23 -0300

    alteração exercicio 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
+  String nome = "Alice";
+
   runApp(
     Center(
-      child: Text(
-        "Olá Flutter",
+      child: RichText(
         textDirection: TextDirection.ltr,
+        text: TextSpan(
+          text: "Olá, ",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.black,
+          ),
+          children: [
+            TextSpan(
+              text: "$nome",
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.double,
+              ),
+            ),
+            TextDirection.ltr
+            TextSpan(
+              text: '\nHoje é sábado',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            TextSpan(
+              text: "! \nBoa noite!",
+            ),
+          ],
+        ),
       ),
-    )
+    ),
   );
 }
